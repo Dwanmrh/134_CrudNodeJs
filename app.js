50eecb6a68586dae34dda9df865c92f604a3535e
RidwanC134 <ridwanhh10@gmail.com>
2024-11-05 20:12:15 +0700

    dbcrud_134

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-// Menggunakan Express
 const express = require("express");
-const todoRoutes = require("./routes/todo.js");
+const todoRoutes = require("./routes/tododb.js"); // Disesuaikan untuk memastikan impor yang benar
 const app = express();
-const port = 3000;
-
-// Middleware untuk parsing JSON
+require("dotenv").config();
+const port = process.env.PORT;
 app.use(express.json());
 
-// Menggunakan routes untuk todos
+// Routes untuk /todos akan ditangani oleh todo.js
 app.use("/todos", todoRoutes);
 
-// Set view engine ke EJS
+// Mengatur view engine menjadi EJS
 app.set("view engine", "ejs");
 
 // Route untuk halaman utama
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index"); // Render view index.ejs
 });
 
-// Route untuk halaman contact
+// Route untuk halaman kontak
 app.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact"); // Render view contact.ejs
 });
 
-// Middleware untuk menangani 404 (halaman tidak ditemukan)
+// Penanganan 404 untuk rute yang tidak ditemukan
 app.use((req, res) => {
   res.status(404).send("404 - Page Not Found");
 });
 
-// Menjalankan server
+// Memulai server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server berjalan di http://localhost:${port}/`);
 });
